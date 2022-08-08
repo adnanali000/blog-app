f1e6e30c46dd416f40a066578ea1eb4d3b04e15b
adnanali000 <adnan.museel123@gmail.com>
2022-08-08 14:42:43 +0500

    client navbar and header work done

---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Header from '../../components/header/Header'
+import './home.css'
+
+const Home = () => {
+  return (
+    <div className='home'>
+        <Header />
+        Home page
+    </div>
+  )
+}
+
+export default Home
